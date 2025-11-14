8ae367469e2307d0a4d0fab5cb22e7f158ce5839
Whisper-of-the-Void <wofthevoid@gmail.com>
2025-11-14 11:27:37 +0500

    Create forum_sections.js

---
+++ config/forum_sections.js
@@ -0,0 +1,12 @@
+// Forum sections configuration
+const GAME_SECTIONS = {
+    // Добавьте ID ваших игровых разделов
+    roleplay: [7],
+};
+
+const FLOOD_SECTIONS = {
+    offtopic: [9],
+    evenings: [10],
+    diaries: [11],
+    contest: [12]
+};
