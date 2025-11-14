e9f7d3df6b7200b0b4161a1b389c2dc927576732
Whisper-of-the-Void <wofthevoid@gmail.com>
2025-11-14 11:37:21 +0500

    Update forum_sections.js

--- config/forum_sections.js
+++ config/forum_sections.js
@@ -1,12 +1,23 @@
-// Forum sections configuration
+// config/forum_sections.js
 const GAME_SECTIONS = {
-    // Добавьте ID ваших игровых разделов
-    roleplay: [7],
+    roleplay: [7]  // Ролевые игры - игровые посты
 };
 
 const FLOOD_SECTIONS = {
-    offtopic: [9],
-    evenings: [10],
-    diaries: [11],
-    contest: [12]
+    offtopic: [9],     // Оффтоп
+    evenings: [10],    // Вечеринки
+    diaries: [11],     // Дневники
+    contest: [12]      // Конкурсы
+};
+
+// Для парсера - полный список всех разделов
+const ALL_SECTIONS = {
+    ...GAME_SECTIONS,
+    ...FLOOD_SECTIONS
+};
+
+module.exports = {
+    GAME_SECTIONS,
+    FLOOD_SECTIONS,
+    ALL_SECTIONS
 };
